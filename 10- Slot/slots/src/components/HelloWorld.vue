1656c584ebcdbb9085ab5a36fd816e14b38dea2a
abbasghaith23 <abbasghaith23@gmail.com>
2023-04-17 02:05:31 -0800

    Slots

---
+++ 10- Slot/slots/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <slot :user="user" :favorites="favorites"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      user: {
+        name: 'Abbas',
+        age: 27
+      },
+      favorites: ['Pizza', 'Marbles','Birds']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
