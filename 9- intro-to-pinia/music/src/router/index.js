d831d009e3e39c6d918beab69c9f635cf4106541
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-15 03:24:13 -0800

    Route specific guards

--- 9- intro-to-pinia/music/src/router/index.js
+++ 9- intro-to-pinia/music/src/router/index.js
@@ -18,7 +18,11 @@ const routes= [
     name: 'manage',
     // alias: '/manage',
     path: '/manage-music',
-    component: Manage
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    }
   },
   {
     path: '/manage',
@@ -38,7 +42,6 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   console.log('Global Guard');
-  console.log(to, from);
   
   next();
 });
