106a18e6afff59169a954d0b50e428aa77b74df1
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-15 00:32:15 -0800

    Naming routes

--- 9- intro-to-pinia/music/src/router/index.js
+++ 9- intro-to-pinia/music/src/router/index.js
@@ -2,17 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
- 
+
 const routes= [
   {
+    name: 'home',
     path: '/',
     component: Home, 
   },
   {
+    name: 'about',
     path: '/about',
     component: About
   },
   {
+    name: 'manage',
     path: '/manage',
     component: Manage
   }
