7d0323172cfb47a877805e2a740450308c8975cf
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-14 21:48:50 -0800

    Setting up the login

--- 9- intro-to-pinia/music/src/stores/user.js
+++ 9- intro-to-pinia/music/src/stores/user.js
@@ -24,6 +24,11 @@ export default defineStore('user', {
             });
 
             this.userLoggedIn = true;
+        },
+        async authenticate(values) {
+            await auth.signInWithEmailAndPassword(values.email, values.password);
+
+            this.userLoggedIn = true;
         }
     }
 })
