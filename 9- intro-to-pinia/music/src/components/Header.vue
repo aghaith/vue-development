bf31197c5020fede09e4ffe6ac0edcc4ae510a09
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-15 03:45:26 -0800

    Redirecting after logging out

--- 9- intro-to-pinia/music/src/components/Header.vue
+++ 9- intro-to-pinia/music/src/components/Header.vue
@@ -24,7 +24,7 @@
               <router-link class="px-2 text-white" to="/manage">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
             </li>
           </template>
         </ul>
@@ -47,6 +47,14 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
       console.log(this.modalStore.isOpen)
+    },
+    signOut() {
+      this.userStore.signOut();
+
+      // console.log(this.$route);
+      if (this.$route.name === 'manage') {
+        this.$router.push({ name: 'home' });
+      }
     }
   }
 }
