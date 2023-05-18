8db9693ce3c42e6f553274417475d3ce63ccc2c9
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-18 04:19:58 -0800

    Quering the database

--- 9- intro-to-pinia/music/src/views/Manage.vue
+++ 9- intro-to-pinia/music/src/views/Manage.vue
@@ -134,12 +134,16 @@
 <script>
 // import useUserStore from '@/stores/user';
 import AppUpload from '@/components/Upload.vue';
+import { songsCollection, auth } from '@/includes/firebase';
 
 export default {
   name: 'Manage',
   components: {
     AppUpload,
   },
+  async created() {
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get();
+  }
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
   //   next();
