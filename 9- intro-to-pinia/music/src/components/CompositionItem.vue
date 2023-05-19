6cc036055b47fe99fad141e6c8ac97c13f22c1a6
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-18 22:53:11 -0800

    Editing a song

--- 9- intro-to-pinia/music/src/components/CompositionItem.vue
+++ 9- intro-to-pinia/music/src/components/CompositionItem.vue
@@ -15,6 +15,13 @@
             </button>
         </div>
         <div v-show="showForm">
+            <div 
+                class="text-white text-center font-bold p-4 mb-4" 
+                v-if="show_alert"
+                :class="alert_variant"
+            >
+                {{ alert_message }}
+            </div>
             <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
                 <div class="mb-3">
                     <label class="inline-block mb-2">Song Title</label>
@@ -36,10 +43,19 @@
                     />
                     <ErrorMessage class="text-red-600" name="genre" />
                 </div>
-                <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
+                <button 
+                    type="submit" 
+                    class="py-1.5 px-3 rounded text-white bg-green-600"
+                    :disabled="in_submission"
+                >
                     Submit
                 </button>
-                <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
+                <button 
+                    type="button" 
+                    class="py-1.5 px-3 rounded text-white bg-gray-600"
+                    :disabled="in_submission"
+                    @click.prevent="showForm = false"
+                >
                     Go Back
                 </button>
             </vee-form>
@@ -48,11 +64,21 @@
 </template>
 
 <script>
+import { songsCollection } from '@/includes/firebase';
+
 export default {
     name: 'CompositionItem',
     props: {
         song: {
             type: Object,
+            required: true,
+        },
+        updateSong: {
+            type: Function,
+            required: true
+        },
+        index: {
+            type: Number,
             required: true,
         }
     }, 
@@ -62,12 +88,34 @@ export default {
             schema: {
                 modified_name: 'required',
                 genre: 'alpha_spaces'
-            }
+            },
+            in_submission: false,
+            show_alert: false,
+            alert_variant: 'bg-blue-500',
+            alert_message: 'Please wait! Updating song info.',
         }
     },
     methods: {
-        edit() {
-            console.log('Song edited')
+        async edit(values) {
+            this.in_submission = true;
+            this.show_alert = true;
+            this.alert_variant = 'bg-blue-500';
+            this.alert_message = 'Please wait! Updating song info.';
+
+            try {
+                await songsCollection.doc(this.song.docId).update(values);
+            } catch (error) {
+                this.in_submission = false;
+                this.alert_message = 'bg-red-500';
+                this.alert_message = 'Something went wrong! Try again later';
+                return;
+            }
+
+            this.updateSong(this.index, values);
+
+            this.in_submission = false;
+            this.alert_variant = 'bg-green-500';
+            this.alert_message = 'Success!';
         }
     }
 }
--- 9- intro-to-pinia/music/src/views/Manage.vue
+++ 9- intro-to-pinia/music/src/views/Manage.vue
@@ -17,7 +17,13 @@
             </div>
             <div class="p-6">
               <!-- Composition Items -->
-              <composition-item v-for="song in songs" :key="song.docID" :song="song" />
+              <composition-item 
+                v-for="(song, i) in songs" 
+                :key="song.docID" 
+                :song="song" 
+                :updateSong="updateSong"
+                :index="i"
+              />
             </div>
           </div>
         </div>
@@ -53,6 +59,12 @@ export default {
 
       this.songs.push(song);
     })
+  },
+  methods: {
+    updateSongs(i, values) {
+      this.songs[i].modified_name = values.modified_name;
+      this.songs[i].genre = values.genre;
+    }
   }
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
