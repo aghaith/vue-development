6d9bb0f7f84351a4434d3d2c4e3a48fdf22ec8af
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-19 23:42:25 -0800

    Preparing the form

--- 9- intro-to-pinia/music/src/views/Song.vue
+++ 9- intro-to-pinia/music/src/views/Song.vue
@@ -24,14 +24,25 @@
                 <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
             </div>
             <div class="p-6">
-                <vee-form :validation-schema="schema">
+                <div 
+                    class="text-white text-center font-bold p-4 mb-4" 
+                    v-if="comment_show_alert"
+                    :class="comment_alert_variant"
+                >
+                    {{ comment_alert_message }}
+                </div>
+                <vee-form :validation-schema="schema" @submit="addComment">
                     <vee-form 
                         as="textarea"
                         name="comment"
                         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
                         placeholder="Your comment here..."></vee-form>
                     <ErrorMessage class="text-red-600" name="comment" />
-                    <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block">
+                    <button 
+                        type="submit" 
+                        class="py-1.5 px-3 rounded text-white bg-green-600 block"
+                        :disabled="comment_in_submission"
+                    >
                         Submit
                     </button>
                 </vee-form>
@@ -131,7 +142,11 @@ export default {
             song: {},
             schema: {
                 comment: 'required|min:3'
-            }
+            },
+            comment_in_submission: false,
+            comment_show_alert: false,
+            comment_alert_variant: 'bg-blue-500',
+            comment_alert_message: 'Please wait! Your comment is being submitted'
         }
     },
     async created () {
@@ -143,6 +158,14 @@ export default {
         }
 
         this.song = docSnapshot.data();
+    },
+    methods: {
+        async addComment (values) {
+            this.comment_in_submission = true;
+            this.comment_show_alert = true;
+            this.comment_alert_variant = 'bg-blue-500'
+            this.comment_alert_message = 'Please wait! Your comment is being submitted';
+        }
     }
 }
 </script>
