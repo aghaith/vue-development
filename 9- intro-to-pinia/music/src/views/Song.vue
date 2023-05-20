68fa8bbb8254254eb4b9d86c4814317cbc7ae8a4
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-20 05:02:27 -0800

    Updating the comment count

--- 9- intro-to-pinia/music/src/views/Song.vue
+++ 9- intro-to-pinia/music/src/views/Song.vue
@@ -20,7 +20,7 @@
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
             <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
                 <!-- Comment Count -->
-                <span class="card-title">Comments (15)</span>
+                <span class="card-title">Comments ({{ song.comment_count }})</span>
                 <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
             </div>
             <div class="p-6">
@@ -118,6 +118,10 @@ export default {
             return;
         }
 
+        const { sort } = this.$route.query;
+
+        this.sort = sort === '1' || sort === '2' ? sort : '1';
+
         this.song = docSnapshot.data();
         this.getComments();
     },
@@ -137,6 +141,11 @@ export default {
             }
 
             await commentsCollection.add(comment);
+
+            this.song.comment_count += 1;
+            await songsCollection.doc(this.$route.params.id).update({
+                comment_count: this.song.comment_countS
+            });
 
             this.getComments();
 
@@ -165,6 +174,10 @@ export default {
     },
     watch: {
         sort (newVal) {
+            if (newVal === this.$route.query.sort) {
+                return;
+            }
+
             this.$router.push({
                 query: {
                     sort: newVal,
