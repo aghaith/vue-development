ff394cfa636aa02e071b316eb9c41c05610eb434
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-22 00:33:16 -0800

    Player progress bar

--- 9- intro-to-pinia/music/src/stores/player.js
+++ 9- intro-to-pinia/music/src/stores/player.js
@@ -7,15 +7,22 @@ export default defineStore('player', {
         current_song: {},
         sound: {},
         seek: '00:00',
-        duration: '00:00'
+        duration: '00:00',
+        playerProgress: '0%'
     }),
     actions: {
         async newSong(song) {
+            if (this.sound instanceof Howl) {
+                this.sound.unload();
+            }
+
             this.current_song = song;
+
             this.sound = new Howl({
                 src: [song.url],
                 html5: true,
             });
+            
             this.sound.play();
 
             this.sound.on('play', () => {
@@ -35,6 +42,10 @@ export default defineStore('player', {
         progress () {
             this.seek = helper.formatTime(this.sound.seek());
             this.duration = this.sound.duration();
+
+            this.playerProgress = `${
+                (this.sound.seek() / this.sound.duration()) * 100
+            }%`
 
             if (this.sound.playing()) {
                 requestAnimationFrame(this.progress)
