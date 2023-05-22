7e8f15ea0be3e06dbc2b1ac8e525002673d426a3
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-21 23:13:08 -0800

    Toggling Audio

--- 9- intro-to-pinia/music/src/stores/player.js
+++ 9- intro-to-pinia/music/src/stores/player.js
@@ -9,13 +9,30 @@ export default defineStore('player', {
     actions: {
         async newSong(song) {
             this.current_song = song;
-
             this.sound = new Howl({
                 src: [song.url],
-                html5: true
+                html5: true,
             });
+            this.sound.play();
+        },
+        async toggleAudio() {
+            if (!this.sound.playing) {
+                return;
+            }
+            if (this.sound.playing) {
+                this.sound.pause();
+            } else {
+                this.sound.play();
+            }
+        }
+    },
+    getters: {
+        playing: (state) => {
+            if (state.sound.playing) {
+                return state.sound.playing()
+            }
 
-            this.sound.play()
+            return false
         }
     }
 })
