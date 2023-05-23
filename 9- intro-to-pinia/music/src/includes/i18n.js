2d1078c5ea9a378110f6662d42923889a48d6602
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-23 04:47:43 -0800

    First translation message

---
+++ 9- intro-to-pinia/music/src/includes/i18n.js
@@ -0,0 +1,12 @@
+import { createI18n } from 'vue-i18n';
+import en from '@/locales/en.json';
+import fr from '@/locales/fr.json';
+
+export default createI18n({
+    locale: "fr",
+    fallbackLocale: "en",
+    messages: {
+        en,
+        fr
+    }
+})
