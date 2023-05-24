af911f0e4ab5c6d4a499a68c6344da7c6ee22e3b
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-24 04:21:46 -0800

    Perceived performance

--- 9- intro-to-pinia/music/src/views/Song.vue
+++ 9- intro-to-pinia/music/src/views/Song.vue
@@ -24,10 +24,7 @@
                 <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
                     <!-- Comment Count -->
                     <span class="card-title">
-                        {{ $tc('song.comment_count', song.comment_count, { 
-                                count: song.comment_count 
-                            }) 
-                        }}
+                        {{ $tc('song.comment_count', song.comment_count, { count: song.comment_count }) }}
                     </span>
                     <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
                 </div>
@@ -103,20 +100,25 @@ export default {
             });
         }
     },
-    async created() {
-        const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
+    async beforeRouteEnter(to, from, next) {
+        const docSnapshot = await songsCollection.doc(to.params.id).get();
 
-        if (!docSnapshot.exists) {
-            this.$router.push({ name: 'home' });
-            return;
-        }
+        next((vm) => {
+            if (!docSnapshot.exists) {
+                vm.$router.push({ name: 'home' });
+                return;
+            }
 
-        const { sort } = this.$route.query;
+            const { sort } = vm.$route.query;
 
-        this.sort = sort === '1' || sort === '2' ? sort : '1';
+            // eslint-disable-next-line no-param-reassign
+            vm.sort = sort === '1' || sort === '2' ? sort : '1';
 
-        this.song = docSnapshot.data();
-        this.getComments();
+            // eslint-disable-next-line no-param-reassign
+            vm.song = docSnapshot.data();
+            vm.getComments();
+        });
+
     },
     methods: {
         ...mapActions(usePlayerStore, ['newSong']),
