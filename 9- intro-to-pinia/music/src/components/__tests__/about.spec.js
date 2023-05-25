424989f91fafe8eb292b617019c68bf6a99ffabf
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-25 03:48:32 -0800

    Testing the inner content

--- 9- intro-to-pinia/music/src/components/__tests__/about.spec.js
+++ 9- intro-to-pinia/music/src/components/__tests__/about.spec.js
@@ -1,8 +1,12 @@
-import About from '@/views/About';
-import { mount } from '@vue/test-utils';
+import About from '@/views/About.vue';
+import { shallowMount } from '@vue/test-utils';
 
 describe('About.vue', () => {
     test('renders inner text', () => {
-        const wrapper = mount(About);
+        const wrapper = shallowMount(About, {
+            shallow: true
+        });
+
+        expect(wrapper.text()).toContain('about');
     });
 });
