35a2cb6c2a5e24ea53b379f06f8aab19e89470bc
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-25 01:07:15 -0800

    Writing first test

--- 9- intro-to-pinia/music/vite.config.js
+++ 9- intro-to-pinia/music/vite.config.js
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true
+  },
   plugins: [
     vue(), 
     /** Generate the manifest file. In addition, it will create a service worker */
