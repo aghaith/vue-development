efd1ff9bf6d80a027aba9de43cc85e5d0ca0fc05
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-26 05:10:18 -0800

    The Reactive Function

--- 12- Composition API/reactive-references/src/App.vue
+++ 12- Composition API/reactive-references/src/App.vue
@@ -2,25 +2,35 @@
   <div>
     <p>{{ num }}</p>
     <button type="button" @click.prevent="increment">Click Me</button>
+
+    <p>{{ name }}</p>
   </div>
 </template>
 <script>
-import { ref } from 'vue';
+import { ref, reactive, toRefs } from 'vue';
 
 export default {
   name: 'App',
   setup () {
     let num = ref(0);
 
-    console.log(num);
-
     function increment () {
       num.value++;
     }
 
+    const user = reactive({
+      name: 'Abbas',
+      age: 27
+    });
+
+    setTimeout(() => {
+      user.name = 'IQBAL'
+    }, 3000);
+
     return {
       num,
-      increment
+      increment,
+      ...toRefs(user)
     }
   }
 }
