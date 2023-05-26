5484f166d91af5a9dea6be4310d0319188c9bc20
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-25 23:02:08 -0800

    Testing attributes

--- 9- intro-to-pinia/music/src/components/__tests__/songitem.spec.js
+++ 9- intro-to-pinia/music/src/components/__tests__/songitem.spec.js
@@ -22,4 +22,24 @@ describe('SongItem.vue', () => {
 
         expect(compositionAuthor.text()).toBe(song.display_name);
     });
+
+    test('renders song.docId in id attribute', () => {
+        const song = {
+            docId: 'abc',
+        };
+
+        const wrapper = shallowMount(SongItem, {
+            props: {
+                song,
+            },
+            global: {
+                components: {
+                    "router-link": RouterLinkStub,
+                },
+            },
+        });
+
+        // expect(wrapper.attributes().id).toBe(`song-id-${song.docId}`);
+        expect(wrapper.classes()).toContain(`song-id-${song.docId}`);
+    });
 });
