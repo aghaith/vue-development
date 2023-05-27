4c133a552c0c3a0067cae3e5337b4ff7b1586200
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-27 03:36:56 -0800

    Separation of concerns

--- 13- Component Design Patterns/controlled-components/src/components/EmailInput.vue
+++ 13- Component Design Patterns/controlled-components/src/components/EmailInput.vue
@@ -6,21 +6,22 @@
         :value="email" 
         @input="update($event)"
         :class="{
-            'is-valid':
-                email.length >= 4 && email.length > 0,
-            'is-invalid':
-                email.length < 4 && email.length > 0,
+            'is-valid': validateEmail(email),
+            'is-invalid': !validateEmail(email),
         }"
     />
 </template>
 <script>
+import validateEmail from '../validate-email';
+
 export default {
     name: 'EmailInput',
     props: ['email'],
     methods: {
         update($event) {
             this.$emit('update:email', $event.target.value);
-        }
+        },
+        validateEmail,
     }
 }
 </script>
