5351b3993b71ae748ed6ec3b085006ebab28d6d3
abbasghaith23 <abbasghaith23@gmail.com>
2023-05-26 23:39:42 -0800

    Verifying Reactivity

--- 12- Composition API/reactive-references/src/hooks/number.js
+++ 12- Composition API/reactive-references/src/hooks/number.js
@@ -1,8 +1,15 @@
-import { ref, computed } from 'vue';
+import { ref, computed, isRef, isReactive, reactive } from 'vue';
 
 export const useNumber = () => {
 
     let num = ref(0);
+    const accounts = reactive({
+      checking: 3242,
+      saving: 242
+    })
+
+    console.log(isRef(num));
+    console.log(isReactive(accounts))
 
     function increment () {
       num.value++;
